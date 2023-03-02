b12840dec214c275e8a67337dc268560d3c0f9b9
AhadAli01 <ahadali101aa@gmail.com>
2023-03-02 13:52:03 -0700

    Add range test

--- JFreeChart_Lab3/src/org/jfree/data/RangeTest.java
+++ JFreeChart_Lab3/src/org/jfree/data/RangeTest.java
@@ -113,10 +113,21 @@ public class RangeTest {
 			boolean actualContains = sampleRange.contains(23);
 			assertEquals("22 should exist in range of 20 to 24", expectedContains, actualContains);
 		}
+		
 
-		// Test if a valid integer does not exist in range
+		// Test if a valid integer does not exist in range lower
 		@Test
-		public void testGetContainsNumberNotExists() {
+		public void testGetContainsNumberNotExistsLower() {
+			Range sampleRange = new Range(20, 24);
+
+			boolean expectedContains = false;
+			boolean actualContains = sampleRange.contains(19);
+			assertEquals("19 should not exist in range of 20 to 24", expectedContains, actualContains);
+		}
+
+		// Test if a valid integer does not exist in range upper
+		@Test
+		public void testGetContainsNumberNotExistsUpper() {
 			Range sampleRange = new Range(20, 24);
 
 			boolean expectedContains = false;
