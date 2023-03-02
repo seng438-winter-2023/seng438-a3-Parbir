96b8198a99471d6d4b969c395ea94faeba7a4536
AhadAli01 <ahadali101aa@gmail.com>
2023-03-02 10:43:46 -0700

    Added DataUtilities Additional Tests

--- JFreeChart_Lab3/src/org/jfree/data/DataUtilitiesTest.java
+++ JFreeChart_Lab3/src/org/jfree/data/DataUtilitiesTest.java
@@ -402,6 +402,65 @@ public class DataUtilitiesTest extends DataUtilities {
 		assertEquals("The column total should be 0", expectedColumnTotal, actualColumnTotal, .000000001d);
 	}
 	
+	// Test a valid data parameter with positive values and positive index and other
+		// values in other columns
+		@Test
+		public void calculateColumnTotalPositiveTableAndNullValues() {
+			Mockery mockingContext = new Mockery();
+			final Values2D values = mockingContext.mock(Values2D.class);
+
+			mockingContext.checking(new Expectations() {
+				{
+					one(values).getRowCount();
+					will(returnValue(3));
+
+					one(values).getValue(0, 0);
+					will(returnValue(null));
+					one(values).getValue(0, 1);
+					will(returnValue(null));
+					one(values).getValue(0, 2);
+					will(returnValue(null));
+
+					one(values).getValue(1, 1);
+					will(returnValue(null));
+
+					one(values).getValue(2, 1);
+					will(returnValue(null));
+				}
+			});
+
+			double expectedColumnTotal = 0;
+			double actualColumnTotal = DataUtilities.calculateColumnTotal(values, 1);
+			assertEquals("The column total should be 0", expectedColumnTotal, actualColumnTotal, .000000001d);
+		}
+	
+	// Call overloaded method
+	// Testing calculateColumnTotal(Values2D data, int column, int[] validRows)
+
+		// Sample Test Case provided by SENG 438 Lab Document
+		@Test
+		public void calculateColumnTotalForTwoValuesOverloaded() {
+			// setup
+			Mockery mockingContext = new Mockery();
+			final Values2D values = mockingContext.mock(Values2D.class);
+			mockingContext.checking(new Expectations() {
+				{
+					one(values).getRowCount();
+					will(returnValue(2));
+					one(values).getValue(0, 0);
+					will(returnValue(7.5));
+					one(values).getValue(1, 0);
+					will(returnValue(2.5));
+				}
+			});
+			// exercise
+			int[] validRows = {0, 1};
+			double result = DataUtilities.calculateColumnTotal(values, 0, validRows);
+			// verify
+			assertEquals(result, 10.0, .000000001d);
+			// tear-down: NONE in this test method
+		}
+	
 	 //Parbir Code
 	 //createNumberArray2D(double[][] data) Tests
 	 
